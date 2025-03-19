a8f8e1844bc7a4d9d7174cf813ba422a37e0faac
unknown <sushant_chaturvedi@softprodigy.com>
2025-03-19 18:58:43 +0530

    Book Assignment 2

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -8,11 +8,7 @@ router.get("/", bookController.getAllBooks); //Use Booksdb to use an objects and
 //2: Get book by ID
 router.get("/:id", bookController.getBookById);
 
-// 3: Get books by Author
-router.get("/author/:author", bookController.getBooksByAuthor);
 
-// 4: Get books by Title
-router.get("/title/:title", bookController.getBooksByTitle);
 
 //5: Get books Review
 router.get("/:id/reviews", bookController.getBookReview);
